61a22279fe3b4e1d1fbacaaedb02387000c3a710
Martin Juul <code@juul.xyz>
2020-02-02 12:06:43 +0100

    Upgraded syntax to avoid deprecations

--- node-server.js
+++ node-server.js
@@ -1,54 +1,55 @@
-var http = require("http"),
-  url = require("url"),
-  path = require("path"),
-  fs = require("fs");
-port = process.argv[2] || 8888;
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
+
+const httpListenAddr = '0.0.0.0:8878'
 
 http
   .createServer(function(request, response) {
-    var uri = url.parse(request.url).pathname,
-      filename = path.join(process.cwd(), "docs", uri);
+    let uri = url.parse(request.url).pathname,
+      filename = path.join(process.cwd(), 'docs', uri)
 
-    var extname = path.extname(filename);
-    var contentType = "text/html";
+    const extname = path.extname(filename)
+    let contentType = 'text/html'
     switch (extname) {
-      case ".js":
-        contentType = "text/javascript";
-        break;
-      case ".css":
-        contentType = "text/css";
-        break;
-      case ".ico":
-        contentType = "image/x-icon";
-        break;
-      case ".svg":
-        contentType = "image/svg+xml";
-        break;
+      case '.js':
+        contentType = 'text/javascript'
+        break
+      case '.css':
+        contentType = 'text/css'
+        break
+      case '.ico':
+        contentType = 'image/x-icon'
+        break
+      case '.svg':
+        contentType = 'image/svg+xml'
+        break
     }
 
-    fs.exists(filename, function(exists) {
+    fs.access(filename, exists => {
       if (!exists) {
-        response.writeHead(404, { "Content-Type": "text/plain" });
-        response.write("404 Not Found\n");
-        response.end();
-        return;
+        response.writeHead(404, { 'Content-Type': 'text/plain' })
+        response.write('404 Not Found\n')
+        response.end()
+        return
       }
 
-      if (fs.statSync(filename).isDirectory()) filename += "/index.html";
+      if (fs.statSync(filename).isDirectory()) filename += '/index.html'
 
-      fs.readFile(filename, "binary", function(err, file) {
+      fs.readFile(filename, 'binary', (err, file) => {
         if (err) {
-          response.writeHead(500, { "Content-Type": "text/plain" });
-          response.write(err + "\n");
-          response.end();
-          return;
+          response.writeHead(500, { 'Content-Type': 'text/plain' })
+          response.write(err + '\n')
+          response.end()
+          return
         }
-        response.writeHead(200, { "Content-Type": contentType });
-        response.write(file, "binary");
-        response.end();
-      });
-    });
+        response.writeHead(200, { 'Content-Type': contentType })
+        response.write(file, 'binary')
+        response.end()
+      })
+    })
   })
-  .listen(parseInt(port, 10));
+  .listen(httpListenAddr)
 
-console.log("WebUI Aria2 Server is running on http://localhost:" + port);
+console.log(`Started on ${httpListenAddr}`)
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,72 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path')
+const webpack = require('webpack')
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
-const BUILD_DIR = path.join(__dirname, "docs");
-const APP_DIR = path.join(__dirname, "src", "js");
+const BUILD_DIR = path.join(__dirname, 'docs')
+const APP_DIR = path.join(__dirname, 'src', 'js')
 
 const config = {
   entry: {
-    app: APP_DIR + "/app.js"
+    app: APP_DIR + '/app.js',
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.svg$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "flags/"
-            }
-          }
-        ]
-      }
-    ]
+              name: '[name].[ext]',
+              outputPath: 'flags/',
+            },
+          },
+        ],
+      },
+    ],
   },
   output: {
     path: BUILD_DIR,
-    filename: "[name].js"
+    filename: '[name].js',
   },
   plugins: [
-    new CleanWebpackPlugin(["docs"]),
+    new CleanWebpackPlugin(['docs']),
     new webpack.ProvidePlugin({
-      "window.jQuery": "jquery",
-      jQuery: "jquery",
-      $: "jquery"
+      'window.jQuery': 'jquery',
+      jQuery: 'jquery',
+      $: 'jquery',
     }),
     new HtmlWebpackPlugin({
-      template: "src/index-template.html",
-      inject: "head"
+      template: 'src/index-template.html',
+      inject: 'head',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css"
+      filename: '[name].css',
     }),
-    new WorkboxPlugin.GenerateSW()
+    new WorkboxPlugin.GenerateSW(),
   ],
   optimization: {
     splitChunks: {
       cacheGroups: {
-        commons: { test: /[\\/]node_modules[\\/]/, name: "vendor", chunks: "all" }
-      }
-    }
+        commons: { test: /[\\/]node_modules[\\/]/, name: 'vendor', chunks: 'all' },
+      },
+    },
   },
   resolve: {
-    modules: [
-      path.resolve("./"),
-      path.resolve("./node_modules"),
-      path.resolve("./src/js"),
-      path.resolve("./src/scss")
-    ]
-  }
-};
+    modules: [path.resolve('./'), path.resolve('./node_modules'), path.resolve('./src/js'), path.resolve('./src/scss')],
+  },
+}
 
-module.exports = config;
+module.exports = config
